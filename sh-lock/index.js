08814f45aa6783c73c9c1fd1bf506a25152dad3a
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2022-04-15 12:46:42 -0500

    update6810

--- sh-lock/index.js
+++ sh-lock/index.js
@@ -1,17 +1,17 @@
-var password = window. prompt("Enter your password: ");
-if (password === "3377") {
+var password = window. prompt("Enter your key: ");
+if (password === "3377b4") {
       alert("wellcome sawyer47");
       window.location.replace("https://sawyer47altf4.github.io/project-gamma/sh-lock/sh/index.html");
           
- } else if (password === "ac11") {
+ } else if (password === "ac11b7") {
       alert("wellcome art man");
       window.location.replace("https://sawyer47altf4.github.io/project-gamma/sh-lock/sh/index.html");
       
- } else if (password === "5e41") {
+ } else if (password === "5e41d1") {
     alert("wellcome bata tester");
     window.location.replace("https://sawyer47altf4.github.io/project-gamma/sh-lock/sh/index.html");
 
- } else if (password === "30f3") {
+ } else if (password === "388e85") {
     alert("dont give thes key to any one are i will delete this key");
     window.location.replace("https://sawyer47altf4.github.io/project-gamma/sh-lock/sh/index.html"); 
  } else {
