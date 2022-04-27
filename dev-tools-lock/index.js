3f1bb77ccf883067d9174edaa2700ae47733f5cb
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2022-04-27 12:45:35 -0500

    dev tools

---
+++ dev-tools-lock/index.js
@@ -0,0 +1,13 @@
+var password = window. prompt("Enter your key: ");
+if (password === "3377b4") {
+      alert("wellcome sawyer47");
+      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+          
+ } else if (password === "ac11b7") {
+      alert("wellcome art man");
+      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+      
+ } else {
+        alert("you dont belong here")
+        window.location.replace("https://sawyer47altf4.github.io./project-gamma/index.html")
+ }
