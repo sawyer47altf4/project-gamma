089e31e9ea3cc00a96ba0e2a0654e628e827ffa3
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2022-04-27 12:57:56 -0500

    dev tools2

--- dev-tools-lock/index.js
+++ dev-tools-lock/index.js
@@ -1,13 +1,13 @@
 var password = window. prompt("Enter your key: ");
 if (password === "3377b4") {
       alert("wellcome sawyer47");
-      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+      window.location.replace("https://sawyer47altf4.github.io/project-gamma/dev-tools-lock/dev-tools/index.html");
           
  } else if (password === "ac11b7") {
       alert("wellcome art man");
-      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+      window.location.replace("https://sawyer47altf4.github.io/project-gamma/dev-tools-lock/dev-tools/index.html");
       
  } else {
-        alert("you dont belong here")
+        alert("you dont belong here", "get out of here")
         window.location.replace("https://sawyer47altf4.github.io./project-gamma/index.html")
  }
