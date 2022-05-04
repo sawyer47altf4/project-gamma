5bc4b8c73368db1c3688c6e7e6f77023581ebfa6
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2022-05-04 13:37:26 -0500

    icon update421678

--- index.js
+++
@@ -1,9 +0,0 @@
-var password = window. prompt("who made this site: ");
-if (password === "sawyer47") {
-      alert("wellcome");
-      window.location.replace("https://sawyer47altf4.github.io/project-gamma/sh-lock/sh/index.html");
-          
- } else {
-        alert("knowledge is the answer")
-        window.location.replace("https://sawyer47altf4.github.io./project-gamma/index.html")
- }
