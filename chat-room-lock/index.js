827e9614a148e0da62c625dad5e6f5b2ce3586cc
sawyer47altf4 <68043480+sawyer47altf4@users.noreply.github.com>
2022-05-06 10:30:05 -0500

    fix8586954391

--- chat-room-lock/index.js
+++ chat-room-lock/index.js
@@ -1,19 +1,19 @@
 var password = window. prompt("Enter your key: ");
 if (password === "3377b4") {
       alert("wellcome sawyer47");
-      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+      window.location.replace("https://docs.google.com/document/d/1uU0voh8qbGMj9AUtACuln0cHBr7aZiB-vV3RPgRrc1A/edit?usp=sharing");
           
  } else if (password === "ac11b7") {
       alert("wellcome art man");
-      window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+      window.location.replace("https://docs.google.com/document/d/1uU0voh8qbGMj9AUtACuln0cHBr7aZiB-vV3RPgRrc1A/edit?usp=sharing");
       
  } else if (password === "5e41d1") {
     alert("wellcome bata tester");
-    window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html");
+    window.location.replace("https://docs.google.com/document/d/1uU0voh8qbGMj9AUtACuln0cHBr7aZiB-vV3RPgRrc1A/edit?usp=sharing");
 
  } else if (password === "ctrl") {
     alert("wellcome to the room");
-    window.location.replace("https://sawyer47altf4.github.io/project-gamma/chat-room-lock/chat-room/index.html"); 
+    window.location.replace("https://docs.google.com/document/d/1uU0voh8qbGMj9AUtACuln0cHBr7aZiB-vV3RPgRrc1A/edit?usp=sharing"); 
  } else {
         alert("you dont belong here")
         window.location.replace("https://sawyer47altf4.github.io./project-gamma/index.html")
